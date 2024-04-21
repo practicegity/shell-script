19cf00401d87065c951cd5dadb4a235df6fedb4e
Raman <ramanji.usa@gmail.com>
2024-04-21 11:32:24 -0500

    this commit is regarding to username and password

---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo " please enter user name "
+read username # here username is variable
+echo " please enter password"
+read password
+echo "user name is:$ username, password is :$passwd"
