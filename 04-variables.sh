f49b899a5cb2b22d5082c74f606ff2cda4f5cf43
Raman <ramanji.usa@gmail.com>
2024-04-21 11:18:51 -0500

    arguments/parameters

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+declaring variables.
+person1=$1
+person2=$2
+
+referring variables.
+echo "$person1: hello $person2; how are you"
+echo "$person2:: hi , i am fine "
+echo "$person1:: hi , i am good "
+echo "$person2: i am thinking i want to learn devops"
+echo  "$person1: I am too learning devops"
+arguments/parameters we can give the values from outside
