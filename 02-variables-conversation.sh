3cb8eb3aed70491ecb1df567462f2d454d8a5ad5
Raman <ramanji.usa@gmail.com>
2024-04-21 10:54:31 -0500

    this is variable commit

---
+++ 02-variables-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1=Ramesh
+person2=suresh
+
+
+echo "$person1: hello $person2; how are you"
+echo "$person2:: hi , i am fine "
+echo "$person1:: hi , i am good "
+echo "$person2: i am thinking i want to learn devops"
