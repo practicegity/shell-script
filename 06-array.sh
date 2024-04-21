93f785aa8c33ed0075c0505ce5f49e2108d44eb1
Raman <ramanji.usa@gmail.com>
2024-04-21 13:51:50 -0500

    this is array commit

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+movies=("djtillu" "rrr" "murari")
+list always starts with zero
+size is same
+dj tillu is 0 rrr is 1 murari is 2
+size of above array is 3
+but indexes are 2 (0,1,2)
+echo "first movie name is=$movie(0)"
+"
